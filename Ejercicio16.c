df5a46312ca2742479445e120bb8a7a43d24636c
isaacmartinez3133-dot <isaacmartinez3133@gmail.com>
2026-05-14 22:25:15 -0600

    Update Ejercicio16.c

--- Ejercicio16.c
+++ Ejercicio16.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+void main ()
+{
+    int a=0, i;
+    int arreglo [100];
+    for (i=0; i<100; i++)
+    {
+        arreglo[i] = a;
+        a = a + 2; 
+    }
+    for (i=0; i<100; i++)
+    {
+        printf("%d\n", arreglo[i]);
+    }
+}
