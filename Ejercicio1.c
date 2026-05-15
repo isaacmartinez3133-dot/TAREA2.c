95f6d3992a4affe2f6cbca563e5816e7bf218b91
isaacmartinez3133-dot <isaacmartinez3133@gmail.com>
2026-05-14 22:43:01 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -2,12 +2,15 @@
 int main ()
 {
   int a, b, c, d, f;
-  printf("Ingresa el valor de a: ");  
-  printf("Ingresa el valor de b: ");
+  printf("Ingresa el valor de a: "); 
   scanf("%d", &a);
+  printf("Ingresa el valor de b: ");
   scanf("%d", &b);
   c = a + b;
   d = a - b;
   f = a * b;
+  printf("la suma de a y b es igual a %d\n", c)
+  printf("la resta de a y b es igual a %d\n", d)
+  printf("la multiplicacion de a y b es igual a %d\n", f)
   return 0;
 }
