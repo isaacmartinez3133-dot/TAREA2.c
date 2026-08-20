23b2afe1026660ba5762f3a71e601595a2695612
isaacmartinez3133-dot <isaacmartinez3133@gmail.com>
2026-08-19 21:26:50 -0600

    Update Ejericicio13.c

--- Ejericicio13.c
+++ Ejericicio13.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
-void main ()
-{
-    int i, sum = 0, N;
-    printf("Ingresa el tamaño del arreglo\n");
-    scanf("%d", &N);
-    int arreglo[N]; 
-    for (i=0; i<N; i++)
-    {
-        printf("Ingrese el valor de la posicion %d\n", i+1);
-        scanf("%d", &arreglo[i]);
-        sum = sum + arreglo[i];
+#include <string.h>
+void imprimir_oculto(const char *texto_encriptado) {
+    for (int i = 0; texto_encriptado[i] != '\0'; i++) {
+        putchar(texto_encriptado[i] - 3);
     }
-    printf("La suma de los elementos es %d", sum);
+    printf("\n");
+}
+
+int main() {
+    char nombre[100];
+
+    printf("INTRODUCE TU NOMBRE PREFERIDO EN MAYÚSCULAS: ");
+    scanf("%99s", nombre);
+    while (getchar() != '\n');
+
+    if (strcmp(nombre, "MAYELA") == 0 || strcmp(nombre, "MAYE") == 0) {
+        printf("\n");
+        imprimir_oculto("Huhv#ho#dpru#gh#pl#ylgd");
+        
+        printf("\n picale al enter para seguir ");
+        getchar();
+        imprimir_oculto("Fdgd#gld#d#wx#odgr#hv#lqfuhlboh1");
+
+        printf("\n otra vez picale");
+        getchar();
+
+        imprimir_oculto("Judfldg#sru#kdfhu#pl#pxqgr#pdv#erqlwr1");
+
+        printf("\n una ultima vez");
+        getchar();
+        imprimir_oculto("Wh#dpr#frq#wrgr#pl#frudcrq1#?6");
+
+    } else 
+    {
+        printf("no eres a quien espero\n");
+    }
+
+    return 0;
 }
